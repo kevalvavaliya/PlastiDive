a80d5fe7b6e3c5eb491aa024704505336b130ccc
kevalvavaliya <kevalvavaliya@gmail.com>
2022-10-22 17:34:03 +0530

    Initial commit added app structure

---
+++ android/app/src/main/kotlin/com/example/plastidive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.plastidive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+
+
+Future<void> main() async {
+ 
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
